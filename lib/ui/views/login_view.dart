2172f87fb0ada43ddde941d5f2fcbcf2a7f086d2
FaytMx <silverzero55@gmail.com>
2021-07-04 21:05:56 -0500

    optimizaciones

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -83,7 +83,8 @@ class LoginView extends StatelessWidget {
                     LinkText(
                       text: 'Nueva cuenta',
                       onPressed: () {
-                        Navigator.pushNamed(context, Flurorouter.registerRoute);
+                        Navigator.pushReplacementNamed(
+                            context, Flurorouter.registerRoute);
                       },
                     ),
                   ],
